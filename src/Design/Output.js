23408fbc4baee0a214ea51ebd18abbcb14520eb7
TahminaTania <1611414642@northsouth.edu>
2022-09-05 17:09:42 +0600

    ...

--- src/Design/Output.js
+++ src/Design/Output.js
@@ -1,33 +1,30 @@
 import React from 'react';
-import benefits from '../db.json';
-import Necessity from '../necessity.json';
-import point from '../point.json';
 import classes from './Output.module.css';
 import id from './Output.module.css';
 import UserInputForm from './UserInputForm';
 import { useState, useEffect,useRef } from 'react';
 
-export default function Output({cetagory} , {keyword}) {
-  const [topic,SetTopic] =useState(point);
+export default function Output({cetagory ,keyword}) {
+  // const [topic,SetTopic] =useState(point);
 
   console.log("Recieved keyword: ",keyword)
   console.log("Recieved category: ",cetagory)
 
-  useEffect(() => {
-    if(cetagory=== '1'){
-      SetTopic(benefits)
-      console.log("useeffect value: ", topic[1].word)
-      let string = topic[1].word;
-      const Word = string.replace("{}",keyword);
-      console.log("Replaced value: ", Word)
-    }
-    else if(cetagory=== '3'){
-      SetTopic(Necessity)
-    }
-    else{
-      SetTopic(point)
-}
-  }, [cetagory]);
+//   useEffect(() => {
+//     if(cetagory=== '1'){
+//       SetTopic(benefits)
+//       // console.log("useeffect value: ", topic[1].word)
+//       // let string = topic[1].word;
+//       // const Word = string.replace("{}",keyword);
+//       // console.log("Replaced value: ", Word)
+//     }
+//     else if(cetagory=== '3'){
+//       SetTopic(Necessity)
+//     }
+//     else{
+//       SetTopic(point)
+// }
+//   }, [cetagory]);
 
 
 
@@ -37,7 +34,10 @@ export default function Output({cetagory} , {keyword}) {
            
               <div>
                     {
-                      topic.map( sub => {
+                      cetagory.map( sub => {
+                        let string = sub.word;
+                        const Word = string.replace("{}",keyword);
+                        sub.word=Word
                        return(
                       <div  className='text-center' id={id.title}> 
                         <span key={sub.id}>{sub.word}</span>
--- src/Design/UserInputForm.js
+++ src/Design/UserInputForm.js
@@ -3,34 +3,47 @@ import classes from './UserInputForm.module.css';
 import { useState } from 'react';
 import Output from './Output';
 
+import benefits from '../db.json';
+import Necessity from '../necessity.json';
+import point from '../point.json';
+
+
+
 
 export default function UserInputForm(props) {
   const [keyword,SetKeyWord] =useState();
-  const [cetagory,SetCetagory] =useState();
+  const [cetagory,SetCetagory] =useState(benefits);
 
   let Sentences =(e)=>{
     e.preventDefault();
-    SetCetagory(e.target.value)
-    console.log('chatagory is:', cetagory);
-    console.log('value is:', keyword);
+    if(cetagory=== '2'){
+      SetCetagory(point)
+    }
+   // console.log('chatagory value is:', cetagory);
+    //console.log('Sentence value is:', keyword);
   }
 
-  function handleCategory(e) {
-    SetCetagory(e.target.value);
-    console.log(cetagory)
-  }
+  // function handleCategory(e) {
+  //   SetCetagory(e.target.value);
+  //   // console.log(cetagory)
+  // }
+  
+  // function handleKeyword(e) {
+  //   SetKeyWord(e.target.value);
+  //   // console.log(cetagory)
+  // }
 
 
   return (
     <div>
         <div className="col-md-10 offset-1">
             <h6>Fill up those things</h6>
-            <form onSubmit={keyword}> 
+            <form> 
                 <div className="row">
             
                       <div className="col-md-4">
                         <label className="form-label mt-4">Choose Category</label> 
-                          <select className="form-select" defaultValue={cetagory} onChange={handleCategory} >
+                          <select className="form-select" defaultValue={cetagory} onChange={(e) => SetCetagory(e.target.value)} >
                             <option value="0">Topic</option>
                             <option value="1">Benefit</option>
                             <option value="2">Pin Point</option>
@@ -47,7 +60,7 @@ export default function UserInputForm(props) {
                         </div>
                         <div className="col-md-2">
                           <div className='mt-5'></div> 
-                          <button className="btn btn-primary btn"  onClick={Sentences} >Generate</button>
+                          <button type="submit" className="btn btn-primary btn"  onClick={Sentences} >Generate</button>
                         </div>
                      </div>
                  
@@ -66,3 +79,6 @@ export default function UserInputForm(props) {
     </div>
   )
 }
+
+
+//
